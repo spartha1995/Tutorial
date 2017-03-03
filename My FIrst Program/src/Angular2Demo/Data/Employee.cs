5593aa3268b8e145ea5fcc4d251debf0ed12f7c3
Partha Sarathi <partha@promact.com>
2017-03-03 17:51:14 +0530

    Demo file

---
+++ My FIrst Program/src/Angular2Demo/Data/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2Demo.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ My FIrst Program/src/Angular2Demo/wwwroot/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app/app.component.js" />
+/// <reference path="app/app.module.js" />
+/// <reference path="app/main.js" />
+/// <reference path="systemjs.config.js" />
